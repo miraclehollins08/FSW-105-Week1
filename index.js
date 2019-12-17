044043cf009a90a7dfffa207f0956eb2554fb69f
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2019-12-16 22:13:59 -0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+var name = "Marvin"
+console.log(name)
+
+var age = "18"
+console.log("18")
+
+var ishealthy = true
+var veryhealthy = false
+
+var items = [ 'Apple', 'PickleEggs', 'Grapes', 'Crush12Pack']
+console.log(items)
+
+var object = {
+    item: 'Apple',
+    price: 0.99,
+    count: 1,
+    color: 'Red',
+    ishealthy: true,
+}
+console.log(object)
+
+var object = {
+    item: 'PickleEggs',
+    price: 2.99,
+    count: 4,
+    color: 'Pink',
+    veryhealthy: false,
+}
+console.log(object)
+
+var object = {
+    item: 'Grapes',
+    price: 1.99,
+    count: 2,
+    color: 'Green',
+    ishealthy: true,
+}
+console.log(object)
+
+var object = {
+    item: 'Crush12Pack',
+    price: 3.95,
+    count: 6,
+    color: 'Orange',
+    veryhealthy: false,
+}
+console.log(object)
